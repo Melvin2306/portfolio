7bdbe5bc916a89b1621ce6924eed41ee2c1cecd4
Melvin2306 <melvin.rinkleff@gmx.de>
2024-03-23 23:28:03 +0100

    general next setup

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
+import { ThemeProvider } from '@/components/theme/ThemeProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Welcome to MelvinOS',
+  description: 'Portfolio page of Melvin Rinkleff',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
