bc9b0a09c4175928749d64ff23ab3595be76a4a7
Melvin2306 <melvin.rinkleff@gmx.de>
2024-03-24 12:46:43 +0100

    added TerminalWelcome to TerminalDisplay

--- components/terminal/TerminalDisplay.tsx
+++ components/terminal/TerminalDisplay.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import TerminalWelcome from './TerminalWelcome';
 
 function TerminalDisplay() {
-  return <div className='flex flex-1'>Display</div>;
+  return (
+    <div className='flex flex-1'>
+      <TerminalWelcome />
+    </div>
+  );
 }
 
 export default TerminalDisplay;
