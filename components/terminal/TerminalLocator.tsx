a6087143ab490880ac1320f3cbf9ae33a9c04f0b
Melvin2306 <melvin.rinkleff@gmx.de>
2024-03-24 12:46:24 +0100

    added content-center

--- components/terminal/TerminalLocator.tsx
+++ components/terminal/TerminalLocator.tsx
@@ -5,7 +5,7 @@ function TerminalLocator() {
   const [user, setUser] = useState('user');
   const [location, setLocation] = useState('location');
   return (
-    <div className='w-3/10'>
+    <div className='w-3/10 content-center'>
       <span className=''>{user}</span>
       <span>@</span>
       <span className=''>{location}</span>
