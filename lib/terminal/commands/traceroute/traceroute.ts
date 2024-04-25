3d69fd0682ab08794fe9c78c471852b9738792d5
Melvin RInkleff <melvin.rinkleff@carbyte.de>
2024-04-26 00:55:23 +0200

    new features

--- lib/terminal/commands/traceroute/traceroute.ts
+++ lib/terminal/commands/traceroute/traceroute.ts
@@ -1,13 +1,20 @@
 import { CommandInput } from '@/types/input';
 
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export function traceroute(input: CommandInput): Array<string> {
   if (input.item === undefined) {
     return ['traceroute: missing host argument'];
   }
+  const ipAddress1 = `${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}`;
+  const ipAddress2 = `${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}`;
+  const ipAddress3 = `${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}.${getRandomInt(255)}`;
   return [
     `traceroute to ${input.item} (${input.item}), 64 hops max, 40 byte packets`,
-    '1  10.65.0.1 (10.65.0.1)  17.082 ms  3.356 ms  3.032 ms',
-    '2  185.199.104.13 (185.199.104.13)  4.052 ms  4.159 ms  3.440 ms',
-    '3  185.199.104.129 (185.199.104.129)  4.391 ms !N *  4.326 ms !N',
+    `1  ${ipAddress1} (${ipAddress1})  ${getRandomInt(12)}.${getRandomInt(999)} ms  ${getRandomInt(12)}.${getRandomInt(999)} ms  ${getRandomInt(12)}.${getRandomInt(999)} ms`,
+    `2  ${ipAddress2} (${ipAddress2})  ${getRandomInt(12)}.${getRandomInt(999)} ms  ${getRandomInt(12)}.${getRandomInt(999)} ms  ${getRandomInt(12)}.${getRandomInt(999)} ms`,
+    `3  ${ipAddress3} (${ipAddress3})  ${getRandomInt(12)}.${getRandomInt(999)} ms !N *  ${getRandomInt(12)}.${getRandomInt(999)} ms !N`,
   ];
 }
--- lib/terminal/commands/uname/uname.ts
+++ lib/terminal/commands/uname/uname.ts
@@ -1,10 +1,27 @@
 import { CommandInput } from '@/types/input';
 
 export function uname(input: CommandInput): Array<string> {
-  // Use uname -s to display the kernel name.
-  // Use uname -n to display the hostname.
-  // Use uname -r to display the kernel release.
-  // Use uname -v to display the kernel version.
-  // Use uname -m to display the machine hardware name.
+  if (input.flags?.includes('-a')) {
+    return [
+      `Darwin rinkleff.dev.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:11:08 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T8122 arm64`,
+    ];
+  }
+  if (input.flags?.includes('-s')) {
+    return [`Darwin`];
+  }
+  if (input.flags?.includes('-n')) {
+    return [`rinkleff.dev`];
+  }
+  if (input.flags?.includes('-r')) {
+    return [`23.4.0`];
+  }
+  if (input.flags?.includes('-v')) {
+    return [
+      `Darwin Kernel Version 23.4.0: Fri Mar 15 00:11:08 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T8122`,
+    ];
+  }
+  if (input.flags?.includes('-m')) {
+    return [`arm64`];
+  }
   return [`${input.item}`];
 }
