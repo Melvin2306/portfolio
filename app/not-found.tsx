d34928ff3680e68c2cad4a05c8c63f1ddf20e6f7
Melvin RInkleff <melvin.rinkleff@carbyte.de>
2024-04-25 19:53:09 +0200

    aisjfgiuhsa

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,31 @@
+'use client';
 import TerminalWindow404 from '@/components/404/TerminalWindow404';
 import Footer from '@/components/Footer';
+import { useEffect } from 'react';
 
 export default function NotFound() {
+  const activeTitle = '404 Page Not Found';
+  const inactiveTitle = 'Don`t leave me...';
+
+  useEffect(() => {
+    const handleBlur = () => {
+      document.title = inactiveTitle;
+    };
+
+    const handleFocus = () => {
+      document.title = activeTitle;
+    };
+
+    window.addEventListener('blur', handleBlur);
+    window.addEventListener('focus', handleFocus);
+
+    document.title = activeTitle;
+
+    return () => {
+      window.removeEventListener('blur', handleBlur);
+      window.removeEventListener('focus', handleFocus);
+    };
+  }, []);
   return (
     <main className='flex h-screen flex-col gap-2 p-4'>
       <TerminalWindow404 />
